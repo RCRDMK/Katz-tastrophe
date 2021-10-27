1012e1d504dfa7e008d94a90de31b0ffd5c8d3d5
Ricardo <ricardo.mook@uni-oldenburg.de>
2021-10-27 16:11:46 +0200

    View und Presenter erstellt

---
+++ src/main/java/Main.java
@@ -0,0 +1,7 @@
+import presenter.ClientPresenter;
+
+public class Main {
+    public static void main(String[] args) {
+        ClientPresenter.main(args);
+    }
+}
---
+++ src/main/java/presenter/ClientPresenter.java
@@ -0,0 +1,17 @@
+package presenter;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class ClientPresenter extends Application {
+
+    public static final String fxml = "/fxml/ClientView.fxml";
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    public void start(Stage stage) throws Exception {
+
+    }
+}
