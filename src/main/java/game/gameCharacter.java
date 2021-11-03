c300f1424686084acc827e277562a53bd40cbd4a
Ricardo Mook <ricardo.mook@uni-oldenburg.de>
2021-11-03 11:38:52 +0100

    Bilder zu MenuItems hinzugefügt und Ordnerstruktur der images verändert

--- src/main/java/game/gameCharacter.java
+++ src/main/java/game/gameCharacter.java
@@ -1,4 +1,56 @@
 package game;
 
 public class gameCharacter {
+
+    /*void lookHere(){
+
+    }*/
+
+    void moveUp(){
+
+    }
+
+    void moveDown(){
+
+    }
+
+    void moveRight(){
+
+    }
+
+    void moveLeft(){
+
+    }
+
+    void takeCat(){
+
+    }
+
+    void takeDrink(){
+
+    }
+
+    void putCatDown(){
+
+    }
+
+    void putDrinkDown(){
+
+    }
+
+    boolean handsFree(){
+
+        return true;
+    }
+
+    boolean catThere(){
+
+        return true;
+    }
+
+    boolean stepOverCat(){
+
+        return true;
+    }
+
 }
