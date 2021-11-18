69390b8ac688138ce373c05b1b9d937c09515dac
Ricardo Mook <ricardo.mook@uni-oldenburg.de>
2021-11-18 17:48:09 +0100

    Spielfeld wird jetzt viel besser dargestellt

--- src/main/java/controller/GameFieldPanelController.java
+++ src/main/java/controller/GameFieldPanelController.java
@@ -10,12 +10,13 @@ public class GameFieldPanelController {
 
     public GameFieldPanelController(int row, int column) {
         this.gameField = new GameField(row, column);
-        this.gameFieldPanel = new GameFieldPanel(gameField, 7, 7);
+        this.gameFieldPanel = new GameFieldPanel(gameField, 250, 250);
         this.gameField.placeElementsInField(5, 5, "C");
         this.gameField.placeElementsInField(3, 1, "W");
         this.gameField.placeElementsInField(3, 2, "C");
         this.gameField.placeElementsInField(4, 1, "D");
         this.gameField.placeElementsInField(3, 3, "^");
+        this.gameFieldPanel.checkGameField();
     }
 
     public GameFieldPanel getGameFieldPanel() {
--- src/main/java/game/GameFieldPanel.java
+++ src/main/java/game/GameFieldPanel.java
@@ -3,13 +3,14 @@ package game;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.layout.Region;
+import javafx.scene.paint.Color;
 
 public class GameFieldPanel extends Region {
 
     //Anleitung zum Arbeiten mit Canvas'. Auch wie man Bilder einfügt oder diese animiert
     //https://edencoding.com/javafx-canvas/
 
-    int test = 20;
+    final int BORDER = 20;
     private GameField gameField;
     private Canvas canvas;
     private GraphicsContext graCon;
@@ -21,19 +22,33 @@ public class GameFieldPanel extends Region {
 
     public GameFieldPanel(GameField gameField, int height, int width) {
         this.gameField = gameField;
-        this.canvas = new Canvas(height * 10, width * 10);
+        this.canvas = new Canvas(height, width);
         this.graCon = canvas.getGraphicsContext2D();
-        this.setPrefSize(canvas.getWidth(), canvas.getHeight());
+        this.setPrefSize(height, width);
         this.getChildren().add(this.canvas);
-        heightCalculated = this.getPrefHeight() / gameField.getGameField().length;
-        widthCalculated = this.getPrefWidth() / gameField.getGameField()[0].length;
+        heightCalculated = (this.getPrefHeight() - BORDER) / gameField.getGameField().length;
+        widthCalculated = (this.getPrefWidth() - BORDER) / gameField.getGameField()[0].length;
         drawGameField();
     }
 
     void drawGameField() {
         for (int i = 0; i < gameField.getGameField().length; i++) {
             for (int j = 0; j < gameField.getGameField()[0].length; j++) {
-                graCon.fillRect(test + j * widthCalculated, test + i * heightCalculated, widthCalculated, heightCalculated);
+                graCon.setStroke(Color.BLACK);
+                graCon.setFill(Color.RED);
+                graCon.fillRect(BORDER + j * widthCalculated, BORDER + i * heightCalculated, widthCalculated, heightCalculated);
+                graCon.strokeRect(BORDER + j * widthCalculated, BORDER + i * heightCalculated, widthCalculated, heightCalculated);
+            }
+        }
+    }
+
+    public void checkGameField() {
+        for (int i = 0; i < gameField.getGameField().length; i++) {
+            for (int j = 0; j < gameField.getGameField()[0].length; j++) {
+                graCon.setFill(Color.BLACK);
+                if (gameField.gameField[i][j].equals("^")) {
+                    graCon.fillRect(BORDER + j * widthCalculated, BORDER + i * heightCalculated, widthCalculated, heightCalculated);
+                }
             }
         }
     }
