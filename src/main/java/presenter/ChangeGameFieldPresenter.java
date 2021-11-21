848ceb9cab620e03c4f06c42c811e07faab05f1d
Ricardo <ricardo.mook@uni-oldenburg.de>
2021-11-21 12:20:09 +0100

    Neue View und Presenter erstellt

---
+++ src/main/java/presenter/ChangeGameFieldPresenter.java
@@ -0,0 +1,11 @@
+package presenter;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class ChangeGameFieldPresenter extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+}
